0cfae92907848283ddc95a9ba1e5744d7e9bbb68
Mohammad S Hussain <m.s.hussain@outlook.com>
2020-09-08 01:40:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  cars.push << "Bugatti"
+  cars = ["Lambourgini", "Aston Martin", "Toyota", "Lexus"]
+  cars.push("Bugatti")
   
 end
 
