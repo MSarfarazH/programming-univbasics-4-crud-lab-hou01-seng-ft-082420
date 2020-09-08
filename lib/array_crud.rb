74b891ad91108a951632ddeebd46244588d37023
Mohammad S Hussain <m.s.hussain@outlook.com>
2020-09-08 01:36:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  cars.unshift()
+  cars.unshift("Infinity")
   
 end
 
@@ -40,5 +40,5 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-cars[2] = Ehonda
+cars[2] = "Ehonda"
 end
